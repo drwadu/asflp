6ffa3b843db0665020898f7db13ebf0e6e53c80b
drwadu <dominik.rusovac@hotmail.com>
2025-01-06 23:43:19 +0100

    update.

--- interface/asflp.py
+++ interface/asflp.py
@@ -17,9 +17,18 @@ class Flp:
         self.encoding = self.encoding + f"\nic :- {','.join(forbidden_and)},-ic"
         self.has_ic = True
 
+    def add_choice_rule(self, head: list[str], body: list[str]) -> None:
+        b = ",".join(body)
+        x = f"cr_aux_{hash(b)}"
+        self.encoding = self.encoding + f"\n{x} :- {b}"
+        for i, a in enumerate(head):
+            xi = f"{x}_{i}"
+            self.encoding = self.encoding + f"\n{a} :- {','.join([x,f"-{xi}"])}"
+            self.encoding = self.encoding + f"\n{xi} :- -{a}"
+
     def infer(self, input: dict[str, float]) -> dict[str, float]:
         lib = cdll.LoadLibrary(self.lib)
-        lib.hs_init()
+        lib.hs_init(0, 0)
         xs = [f"{k}[{l};{u}]" for k, (l, u) in input.items()]
         if self.has_ic:
             xs += ["ic[0.0;0.0]"]
@@ -40,19 +49,37 @@ class Flp:
 
     def true(self) -> None:
         for f, (l, u) in self.knowledge.items():
-            if not "root" in f and not "ic" in f and not "OR" in f and not "proof" in f:
+            if (
+                not "root" in f
+                and not "ic" in f
+                and not "OR" in f
+                and not "proof" in f
+                and not "cr_aux" in f
+            ):
                 if l + u == 2.0:
                     print(f, l, u)
 
     def false(self) -> None:
         for f, (l, u) in self.knowledge.items():
-            if not "root" in f and not "ic" in f and not "OR" in f and not "proof" in f:
+            if (
+                not "root" in f
+                and not "ic" in f
+                and not "OR" in f
+                and not "proof" in f
+                and not "cr_aux" in f
+            ):
                 if l + u == 0.0:
                     print(f, l, u)
 
     def uncertain(self) -> None:
         for f, (l, u) in self.knowledge.items():
-            if not "root" in f and not "ic" in f and not "OR" in f and not "proof" in f:
+            if (
+                not "root" in f
+                and not "ic" in f
+                and not "OR" in f
+                and not "proof" in f
+                and not "cr_aux" in f
+            ):
                 if l == 0.0 and u == 1.0:
                     print(f, l, u)
 
@@ -64,6 +91,7 @@ class Flp:
                 and not "OR" in f
                 and not "proof" in f
                 and not "AND" in f
+                and not "cr_aux" in f
             ):
                 if l == u:
                     print(round(l, 2), f)
